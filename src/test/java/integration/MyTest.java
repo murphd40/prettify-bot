668162188af393c5a6dcb066b7d4bb7d55ca7f98
DMurphy3 <dmurphy3@ie.ibm.com>
2017-02-17 16:03:20 +0000

    ...

---
+++ src/test/java/integration/MyTest.java
@@ -0,0 +1,25 @@
+package integration;
+
+import java.io.IOException;
+
+import app.workspace.model.WebhookEvent;
+import org.junit.Test;
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class MyTest extends BaseIntegrationTest{
+
+    @Test
+    public void myTest() throws IOException {
+
+        WebhookEvent event = new WebhookEvent();
+        event.setContent("content");
+        Call<Void> call = testRequestService.webhookRequest(event);
+
+        Response<Void> response = call.execute();
+
+        int x = 5;
+
+    }
+
+}
