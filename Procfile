abf09ec578c3757db68e792f825b5958f734fc10
murphd40 <murphd40@gmail.com>
2017-02-19 23:15:35 +0000

    revert

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: build/install/prettify-bot/bin/prettify-bot
--- src/test/java/integration/MyTest.java
+++ src/test/java/integration/MyTest.java
@@ -3,12 +3,10 @@ package integration;
 import java.io.IOException;
 
 import app.workspace.model.WebhookEvent;
-import org.junit.Ignore;
 import org.junit.Test;
 import retrofit2.Call;
 import retrofit2.Response;
 
-@Ignore
 public class MyTest extends BaseIntegrationTest{
 
     @Test
